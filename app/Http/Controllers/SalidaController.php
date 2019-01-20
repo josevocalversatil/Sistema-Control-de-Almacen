87d7aca3f8908878386a14f2be53956fc6da1486
jesusrv1103 <jesus21c.jrv@gmail.com>
2019-01-20 16:35:11 -0600

    necesario

--- app/Http/Controllers/SalidaController.php
+++ app/Http/Controllers/SalidaController.php
@@ -22,130 +22,130 @@ use Illuminate\Support\Collection;
 
 class SalidaController extends Controller
 {
-    
-     public function __construct()
-    {
 
-     $this->middleware('auth');
-    }
+   public function __construct()
+   {
 
-     public function index(Request $request)
-    {
-     
-     if ($request)
-      {
-     	
-     $query=trim($request->get('searchText'));
-    
-     $salidas=DB::table('salida as s')
-    
-    ->join('personal as pe','s.idpersonal','=','pe.idpersonal')
-    ->join('personal as per','s.idpersonal2','=','per.idpersonal')
-     ->join('memo as m','s.idmemo','=','m.idmemo')
-     ->join('detalle_salida as ds','s.idsalida','=','ds.idsalida')
-     ->join('departamento as dep','s.iddepartamento','=','dep.iddepartamento')
+       $this->middleware('auth');
+   }
 
-     ->select('s.idsalida','s.fecha_hora','pe.nombre as personal3','dep.nombre as depa','per.nombre as personal4','m.folio_memo','s.estado')
-     ->where('s.idsalida','LIKE','%'.$query.'%')
-     ->orderBy('s.idsalida','desc')
-     ->groupBy('s.idsalida','s.fecha_hora','pe.nombre','dep.nombre','per.nombre','m.folio_memo','s.estado')
-     ->paginate();
+   public function index(Request $request)
+   {
 
-     return view('almacen.salida.index',['salidas'=>$salidas,'searchText'=>$query]);
-      
-     }
+       if ($request)
+       {
+
+           $query=trim($request->get('searchText'));
+
+           $salidas=DB::table('salida as s')
+
+           ->join('personal as pe','s.idpersonal','=','pe.idpersonal')
+           ->join('personal as per','s.idpersonal2','=','per.idpersonal')
+           ->join('memo as m','s.idmemo','=','m.idmemo')
+           ->join('detalle_salida as ds','s.idsalida','=','ds.idsalida')
+           ->join('departamento as dep','s.iddepartamento','=','dep.iddepartamento')
+
+           ->select('s.idsalida','s.fecha_hora','pe.nombre as personal3','dep.nombre as depa','per.nombre as personal4','m.folio_memo','s.estado')
+           ->where('s.idsalida','LIKE','%'.$query.'%')
+           ->orderBy('s.idsalida','desc')
+           ->groupBy('s.idsalida','s.fecha_hora','pe.nombre','dep.nombre','per.nombre','m.folio_memo','s.estado')
+           ->paginate();
+
+           return view('almacen.salida.index',['salidas'=>$salidas,'searchText'=>$query]);
+
+       }
 
 
-    } 
+   } 
 
-    public function create()
-    {
- 
-       
-        
-    	
-        $personales=DB::table('personal')->where('condicion','=','1')->get();
-         $personales2=DB::table('personal')->where('condicion','=','1')->get();
-        $memos=DB::table('memo')->get();
-        $departamentos=DB::table('departamento')->get();
-        $articulos= DB::table('articulo as art')
-        ->select(DB::raw('CONCAT(art.codigo," ",art.nombre) AS articulo'),'art.idarticulo')
-        ->where('art.estado','=','Activo')
-        ->get();
+   public function create()
+   {
+
+
+
+
+    $personales=DB::table('personal')->where('condicion','=','1')->get();
+    $personales2=DB::table('personal')->where('condicion','=','1')->get();
+    $memos=DB::table('memo')->get();
+    $departamentos=DB::table('departamento')->get();
+    $articulos= DB::table('articulo as art')
+    ->select(DB::raw('CONCAT(art.codigo," ",art.nombre) AS articulo'),'art.idarticulo')
+    ->where('art.estado','=','Activo')
+    ->get();
 
 
 
     return view("almacen.salida.create",["departamentos"=>$departamentos,'memos'=>$memos,'personales'=>$personales,'personales2'=>$personales2,"articulos"=>$articulos]);
 
-    }
+}
 
 
-     public function store (SalidaFormRequest $request)
+public function store (SalidaFormRequest $request)
+{
+
+
+    DB::beginTransaction();
+    $salida= new Salida;
+    $salida->iddepartamento=$request->get('iddepartamento');
+    $salida->idmemo=$request->get('idmemo');
+    $salida->idpersonal=$request->get('idpersonal');
+    $salida->idpersonal2=$request->get('idpersonal2');
+
+    $mytime = Carbon::now('America/Mexico_City');
+    $salida->fecha_hora=$mytime->toDateTimeString();
+    $salida->estado='A';
+    $salida->save();
+
+    $idarticulo = $request->get('idarticulo');
+    $cantidad=$request->get('cantidad');
+
+
+
+    $cont = 0;
+    while($cont <count($idarticulo))
     {
+       $detalle = new DetalleSalida();
+       $detalle->idsalida= $salida->idsalida;
+       $detalle->idarticulo= $idarticulo[$cont];
+       $detalle->cantidad= $cantidad[$cont];
 
-    	
-        DB::beginTransaction();
-        $salida= new Salida;
-        $salida->iddepartamento=$request->get('iddepartamento');
-        $salida->idmemo=$request->get('idmemo');
-        $salida->idpersonal=$request->get('idpersonal');
-        $salida->idpersonal2=$request->get('idpersonal2');
-      
-        $mytime = Carbon::now('America/Mexico_City');
-        $salida->fecha_hora=$mytime->toDateTimeString();
-        $salida->estado='A';
-        $salida->save();
-
-        $idarticulo = $request->get('idarticulo');
-        $cantidad=$request->get('cantidad');
-     
-
-
-        $cont = 0;
-        while($cont <count($idarticulo))
-        {
-        	$detalle = new DetalleSalida();
-        	$detalle->idsalida= $salida->idsalida;
-        	$detalle->idarticulo= $idarticulo[$cont];
-        	$detalle->cantidad= $cantidad[$cont];
-        	
-            $detalle->save();
+       $detalle->save();
        $cont=$cont+1;
-        }
-     
-        DB::commit(); 
-       
-    	
+   }
 
-    	
-    	return Redirect::to('almacen/salida');
-    }
+   DB::commit(); 
 
 
-     public function show($id) 
-     {
-     
-     $salida=DB::table('salida as s')
-    
-    ->join('personal as pe','s.idpersonal','=','pe.idpersonal')
-    ->join('personal as per','s.idpersonal2','=','per.idpersonal')
-     ->join('memo as m','s.idmemo','=','m.idmemo')
-     ->join('detalle_salida as ds','s.idsalida','=','ds.idsalida')
-     ->join('departamento as dep','s.iddepartamento','=','dep.iddepartamento')
+
+
+   return Redirect::to('almacen/salida');
+}
+
+
+public function show($id) 
+{
+
+   $salida=DB::table('salida as s')
+
+   ->join('personal as pe','s.idpersonal','=','pe.idpersonal')
+   ->join('personal as per','s.idpersonal2','=','per.idpersonal')
+   ->join('memo as m','s.idmemo','=','m.idmemo')
+   ->join('detalle_salida as ds','s.idsalida','=','ds.idsalida')
+   ->join('departamento as dep','s.iddepartamento','=','dep.iddepartamento')
 
    ->select('s.idsalida','s.fecha_hora','pe.nombre as personal3','dep.nombre as depa','per.nombre as personal4','m.folio_memo','s.estado')
-     ->where('s.idsalida','=',$id)
-     ->groupBy('s.idsalida','s.fecha_hora','pe.nombre','dep.nombre','per.nombre','m.folio_memo','s.estado')
-     ->first();
+   ->where('s.idsalida','=',$id)
+   ->groupBy('s.idsalida','s.fecha_hora','pe.nombre','dep.nombre','per.nombre','m.folio_memo','s.estado')
+   ->first();
 
-     $detalles=DB::table('detalle_salida as d')
-     ->join('articulo as a','d.idarticulo','=','a.idarticulo' )
-     ->select('a.nombre as articulo','d.cantidad')
-     ->where('d.idsalida','=',$id)
-     ->get();
-     return view('almacen.salida.show',['salida'=>$salida,"detalles"=>$detalles]);
+   $detalles=DB::table('detalle_salida as d')
+   ->join('articulo as a','d.idarticulo','=','a.idarticulo' )
+   ->select('a.nombre as articulo','d.cantidad')
+   ->where('d.idsalida','=',$id)
+   ->get();
+   return view('almacen.salida.show',['salida'=>$salida,"detalles"=>$detalles]);
 
-     }
+}
 
 
 
@@ -158,40 +158,38 @@ public function destroy($id)
 } 
 
 
- public function pdf($id)
- {
+public function pdf($id)
+{
 
-      
- $salida=DB::table('salida as s')
-    
-    ->join('personal as pe','s.idpersonal','=','pe.idpersonal')
-    ->join('personal as per','s.idpersonal2','=','per.idpersonal')
-     ->join('memo as m','s.idmemo','=','m.idmemo')
-     ->join('detalle_salida as ds','s.idsalida','=','ds.idsalida')
-     ->join('departamento as dep','s.iddepartamento','=','dep.iddepartamento')
 
+   $salida=DB::table('salida as s')
+   ->join('personal as pe','s.idpersonal','=','pe.idpersonal')
+   ->join('personal as per','s.idpersonal2','=','per.idpersonal')
+   ->join('memo as m','s.idmemo','=','m.idmemo')
+   ->join('detalle_salida as ds','s.idsalida','=','ds.idsalida')
+   ->join('departamento as dep','s.iddepartamento','=','dep.iddepartamento')
    ->select('s.idsalida','s.fecha_hora','pe.nombre as personal3','dep.nombre as depa','per.nombre as personal4','m.folio_memo','s.estado')
-     ->where('s.idsalida','=',$id)
-     ->groupBy('s.idsalida','s.fecha_hora','pe.nombre','dep.nombre','per.nombre','m.folio_memo','s.estado')
-     ->first();
+   ->where('s.idsalida','=',$id)
+   ->groupBy('s.idsalida','s.fecha_hora','pe.nombre','dep.nombre','per.nombre','m.folio_memo','s.estado')
+   ->first();
 
-     $detalles=DB::table('detalle_salida as d')
-     ->join('articulo as a','d.idarticulo','=','a.idarticulo' )
-     ->select('a.nombre as articulo','d.cantidad')
-     ->where('d.idsalida','=',$id)
-     ->get();
+   $detalles=DB::table('detalle_salida as d')
+   ->join('articulo as a','d.idarticulo','=','a.idarticulo' )
+   ->select('a.nombre as articulo','d.cantidad')
+   ->where('d.idsalida','=',$id)
+   ->get();
 
-     $date = Carbon::now();
-     $fecha=$date->format('d-m-Y');
+   $date = Carbon::now();
+   $fecha=$date->format('d-m-Y');
 
 
 
-      $pdf=PDF::loadView("almacen.salida.invoice",["detalles"=>$detalles,"salida"=>$salida,"fecha"=>$fecha]);
-  return $pdf->stream("Salida.pdf");
+   $pdf=PDF::loadView("almacen.salida.invoice",["detalles"=>$detalles,"salida"=>$salida,"fecha"=>$fecha]);
+   return $pdf->stream("Salida.pdf");
 
- 
 
- }
+
+}
 
 
 
